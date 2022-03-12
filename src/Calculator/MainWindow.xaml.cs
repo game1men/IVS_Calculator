822fb2de398016901a974a217227764e84a95d96
xceska06 <xceska06@stud.fit.vutbr.cz>
2022-03-12 15:10:53 +0100

    plan

--- src/Calculator/MainWindow.xaml.cs
+++ src/Calculator/MainWindow.xaml.cs
@@ -20,6 +20,8 @@ namespace GUI_Application {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+            if () { 
+            }
         }
     }
 }
