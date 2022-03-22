53d5345f20ae1e78d1e899336a0f5b52dc3ac145
Matěj Šmida <momo4cz@gmail.com>
2022-03-22 23:05:35 +0100

    Added few basic functions

--- src/MathLib/MathLib.cs
+++ src/MathLib/MathLib.cs
@@ -1,29 +1,81 @@
-﻿namespace MathLib {
+﻿/**
+ * @brief Math Library for IVS project (Calculator)
+ * 
+ * This library contains basic mathematic functions 
+ * such as sum, difference, multiplication, division and 
+ * more complex mathemati functions such as factorial,
+ * power, root and basic logarithm.
+ * 
+ * @author Matej Smida
+ * 
+ */
+
+namespace MathLib {
     public static class CalcMathLib {
+
+        /**
+         * @brief Sum of two numbers
+         * 
+         * @param a first number to be added
+         * @param b second number to be added
+         * 
+         * @return Sum of numbers a and b
+         */
         public static double Add(double a, double b) {
 
-            //The add function
-            return 1.0;
+            return a + b;
 
         }
+
+        /**
+         * @brief Subtraction of two numbers
+         *
+         * @param a first number
+         * @param b second number
+         *
+         * @return Difference of numbers a and b
+         */
         public static double Sub(double a, double b) {
 
-            //The substraction function
-            return 1.0;
+            return a - b;
 
         }
+
+        /**
+         * @brief Multiplication of two numbers 
+         * 
+         * @param a first number to be multiplied
+         * @param b second number to be multiplied
+         * 
+         * @return Product of two numbers
+         */
         public static double Mul(double a, double b) {
 
-            //The multiplication function
-            return 1.0;
+            return a * b;
 
         }
+
+        /**
+         * @brief Division of two numbers
+         * 
+         * @param a first number (dividend)
+         * @param b first number (divisor)
+         * 
+         * @return quotient of two numbers
+         */
         public static double Div(double a, double b) {
 
-            //the division function
-            return 1.0;
+            double result = a / b;
+            //TODO division by zero Error
+            return result;
 
         }
+
+        /**
+         * @brief
+         * @param
+         * @return 
+         */
         public static double Factorial(double a) {
 
             //The factorial function
@@ -31,10 +83,21 @@
 
 
         }
+
+        /**
+         * @brief
+         * @param
+         * @param
+         * @return
+         */
         public static double Power(double a, double exponent) {
 
-            //The power function
-            return 1.0;
+            double power = a;
+            for(int i = 1; i <exponent; i++) {
+                a *= power;
+            }
+            //TODO Power with Nonnatural exponent
+            return a;
 
 
         }
@@ -42,6 +105,7 @@
 
             //The square root function
             return 1.0;
+            
 
         }
         public static double Log(double a) {
