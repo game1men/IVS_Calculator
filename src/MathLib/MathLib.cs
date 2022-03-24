07f6097931124baca45c9e9c6c684aededb89ee8
JosefUncovsky <64826405+JosefUncovsky@users.noreply.github.com>
2022-03-24 19:59:21 +0100

    Refactor the tests to throw exceptions, mod tests

--- src/MathLib/MathLib.cs
+++ src/MathLib/MathLib.cs
@@ -3,51 +3,94 @@
         public static double Add(double a, double b) {
 
             //The add function
-            return 1.0;
+            return a + b;
 
         }
         public static double Sub(double a, double b) {
 
             //The substraction function
-            return 1.0;
+            return a - b;
 
         }
         public static double Mul(double a, double b) {
 
             //The multiplication function
-            return 1.0;
+            return a * b;
 
         }
         public static double Div(double a, double b) {
 
-            //the division function
-            return 1.0;
+            if (a == 0 || b == 0) {
 
+                throw new DivideByZeroException("Division by zero is not possible.");
+            }
+
+            //The dividing function
+            return a / b;
+
+        }
+        public static double Mod(double a, double b) {
+            //The modulo function
+            if (b == 0) {
+                throw new Exception("Modulo by 0 is not possible.");
+            }
+            return a % b;
         }
         public static double Factorial(double a) {
 
             //The factorial function
-            return 1.0;
+            if (a < 0) {
+                //return double.NaN;
+                throw new Exception("Factorial of negative number is not possible.");
+            }
+            if (a % 1 != 0) {
+                //return double.NaN;
+                throw new Exception("Factorial of decimal number is not possible.");
+            }
+            if (a == 1 || a == 0) {
+                return 1;
+
+            } else {
+                return a * Factorial(a - 1);
+
+            }
 
 
         }
         public static double Power(double a, double exponent) {
 
             //The power function
-            return 1.0;
+            if (exponent < 0) {
+                throw new Exception("Number to the power of a negative number is not possible.");
+            }
+            if (exponent % 1 != 0) {
+                throw new Exception("Number to the power of a decimal number is not possible.");
+
+            }
+            return Math.Pow(a, exponent);
 
 
         }
         public static double Root(double a, double exponent) {
 
             //The square root function
-            return 1.0;
+            if (a < 0) {
+                throw new Exception("Root of a negative number is not possible.");
+            }
+            return Math.Pow(a, 1.0 / exponent);
 
         }
         public static double Log(double a) {
 
             //The logarithm function
-            return 1.0;
+            if (a < 0) {
+                throw new Exception("Logarithm of a negative number is not possible.");
+            }
+            if (a == 0) {
+                throw new Exception("Logarithm of zero is not possible.");
+            }
+            return Math.Log(a);
+
         }
     }
 
