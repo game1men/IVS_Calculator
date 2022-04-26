7596971105225dd53b77f9b12b0b8c0b86ab9e98
Petr Večeřa <petrvecera.personal@gmail.com>
2022-04-26 16:54:57 +0200

    Auto-remove deploy folder before compile

--- src/compile.cmd
+++ src/compile.cmd
@@ -2,4 +2,5 @@
 set home=%~dp0
 set application=Calculator
 
+rd /s /q "%home%%application%\bin\deploy"
 dotnet publish %application% -v m -c Release -p:PublishProfileFullPath="%home%CompileProfile.pubxml" -p:PublishDir="%home%%application%\bin\deploy"
